b365d479f8d858e35208f3b524a0292fbb7227fc
vivek sharma <vivekslim@gmail.com>
2021-02-04 23:03:38 +0530

    Js code added by vivek

---
+++ js/feature.js
@@ -0,0 +1,71 @@
+
+      function function1() {
+        document.getElementById("item10").remove();
+        document.getElementById("item11").remove();
+        document.getElementById("item12").remove();
+        document.getElementById("item13").remove();
+        document.getElementById("item14").remove();
+        document.getElementById("item15").remove();
+        document.getElementById("item7").remove();
+        document.getElementById("item8").remove();
+      }
+
+      var btn = document.getElementById("seo");
+      btn.addEventListener("click", function1);
+
+      function function2() {
+        document.getElementById("item2").remove();
+        document.getElementById("item5").remove();
+        document.getElementById("item12").remove();
+        document.getElementById("item13").remove();
+        document.getElementById("item14").remove();
+        document.getElementById("item15").remove();
+        document.getElementById("item7").remove();
+        document.getElementById("item8").remove();
+      }
+
+      var btn = document.getElementById("content");
+      btn.addEventListener("click", function2);
+
+      function function3() {
+        document.getElementById("item2").remove();
+        document.getElementById("item5").remove();
+        document.getElementById("item12").remove();
+        document.getElementById("item13").remove();
+        document.getElementById("item14").remove();
+        document.getElementById("item15").remove();
+        document.getElementById("item7").remove();
+        document.getElementById("item8").remove();
+      }
+
+      var btn = document.getElementById("mr");
+      btn.addEventListener("click", function3);
+
+      function function4() {
+        document.getElementById("item2").remove();
+        document.getElementById("item5").remove();
+        document.getElementById("item12").remove();
+        document.getElementById("item13").remove();
+        document.getElementById("item14").remove();
+        document.getElementById("item15").remove();
+        document.getElementById("item7").remove();
+        document.getElementById("item17").remove();
+      }
+
+      var btn = document.getElementById("ad");
+      btn.addEventListener("click", function4);
+
+      function function5() {
+        document.getElementById("item2").remove();
+        document.getElementById("item5").remove();
+        document.getElementById("item12").remove();
+        document.getElementById("item13").remove();
+        document.getElementById("item14").remove();
+        document.getElementById("item15").remove();
+        document.getElementById("item7").remove();
+        document.getElementById("item17").remove();
+      }
+
+      var btn = document.getElementById("serm");
+      btn.addEventListener("click", function5);
+   
